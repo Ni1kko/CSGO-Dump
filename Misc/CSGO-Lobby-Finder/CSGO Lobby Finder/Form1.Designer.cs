54d0cead34c85c12e55eddea7491a31807cb0bdb
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2021-12-17 08:12:56 +0000

    Dump: CSGO-Lobby-Finder

---
+++ Misc/CSGO-Lobby-Finder/CSGO Lobby Finder/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace CSGO_Lobby_Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Player = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Lobby = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataSet1 = new System.Data.DataSet();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labeltmr = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Player,
+            this.Lobby,
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(625, 218);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Player
+            // 
+            this.Player.Text = "Player";
+            this.Player.Width = 169;
+            // 
+            // Lobby
+            // 
+            this.Lobby.Text = "Lobby";
+            this.Lobby.Width = 191;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Available";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(435, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Join";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(309, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(582, 238);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Binds";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(372, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Message text";
+            this.label1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labeltmr
+            // 
+            this.labeltmr.Interval = 5000;
+            this.labeltmr.Tick += new System.EventHandler(this.labeltmr_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(624, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(506, 238);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(70, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Auto Join";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 286);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = " CSGO Lobby Finder - Made by Lucky";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Player;
+        private System.Windows.Forms.ColumnHeader Lobby;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer labeltmr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+
+    }
+}
+
---
+++ Misc/CSGO-Lobby-Finder/CSGO Lobby Finder/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace CSGO_Lobby_Finder
+{
+    public partial class Form1 : Form
+    {
+        public static ArrayList players = new ArrayList();
+        public static bool busy = false;
+        public static bool working = false;
+        public static bool update = false;
+        public static int total = 0;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern short GetKeyState(int keyCode);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dataSet1.Tables.Add("players");
+            //Thread t = new Thread(new ThreadStart(checkforlobbies));
+            //t.Start();
+            loadList();
+            sendmessage("Welcome to Lucky's CSGO Lobby Joiner.");
+        }
+        private void sendmessage(string s)
+        {
+            label1.Text = s;
+            labeltmr.Start();
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            addItem(textBox1.Text);
+            sendmessage("Added " + textBox1.Text);
+            textBox1.Clear();
+        }
+        public void addItem(string ply)
+        {
+            if (ply == "")
+                return;
+            String[] s = new String[] { ply, "Unknown", "False" };
+            listView1.Items.Add("").SubItems.AddRange(new String[] { "", "" });
+            players.Add(s);
+            //update = true;
+            Thread t = new Thread(new ParameterizedThreadStart(checkPlayer));
+            t.Start(players.Count-1);
+            updateLine(players.Count - 1);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (update)
+            {
+                updateList();
+                update = false;
+            }
+            label2.Text = Convert.ToString(total);
+        }
+        private void updateList()
+        {
+            try
+            {
+                if (players.Count > 0)
+                {
+                    int i = 0;
+                    foreach (String[] s in players)
+                    {
+                        listView1.Items[i].Text = (s[0]);
+                        listView1.Items[i].SubItems[1].Text = s[1];
+                        if (listView1.Items[i].SubItems[2].Text != s[2] && checkBox2.Checked && s[2] == "True")
+                            joinLobby(Convert.ToInt64(s[1]));
+                        listView1.Items[i].SubItems[2].Text = s[2];
+                        i = i + 1;
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+        private void updateLine(int i)
+        {
+            try
+            {
+                listView1.Items[i].Text = (((string[])players[i])[0]);
+                listView1.Items[i].SubItems[1].Text = (((string[])players[i])[1]);
+                listView1.Items[i].SubItems[2].Text = (((string[])players[i])[2]);
+            }
+            catch (Exception) { }
+        }
+        private void checkPlayer(object inte)
+        {
+            while (this.Visible)
+            {
+                int i = (int)inte;
+                string[] cur = (string[])players[i];
+                long lobid = getplobbyID(cur[0]);
+                cur[2] = "Working";
+                players[i] = cur;
+                if (Convert.ToInt64(lobid) > 0)
+                {
+                    cur[1] = Convert.ToString(lobid);
+                    cur[2] = "True";
+                }
+                else
+                    cur[2] = "False";
+                players[i] = cur;
+                update = true;
+            }
+        }
+        /*private void checkforlobbies()
+        {
+            while (this.Visible)
+            {
+                ArrayList p = players;
+                while (busy) { }
+                working = true;
+                for (int i = 0; i < players.Count; i++ )
+                {
+                    Thread t = new Thread(new ParameterizedThreadStart(checkPlayer));
+                    t.Start(i);
+                }
+                bool wait = true;
+                Thread.Sleep(20);
+                while (wait)
+                {
+                    wait = false;
+                    foreach (string[] s in players)
+                    {
+                        if (s[2] == "Working")
+                            wait = true;
+                    }
+                }
+                //listView1.Items[itm.Index] = itm;
+                Thread.Sleep(100);
+                working = false;
+                update = true;
+            }
+        }
+        private void checkforlobbies1()
+        {
+            while (this.Visible)
+            {
+                ListView lv = null;
+
+                this.Invoke((MethodInvoker)(delegate() { lv = listView1; }));
+                foreach (ListViewItem itm in lv.Items)
+                {
+                    long lobid = getplobbyID(itm.Text);
+                    itm.SubItems[2].Text = "False";
+                    if (Convert.ToInt64(lobid) > 0)
+                    {
+                        itm.SubItems[1].Text = Convert.ToString(lobid);
+                        itm.SubItems[2].Text = "True";
+                    }
+                    this.Invoke((MethodInvoker)(() => listView1.Items[itm.Index] = itm));
+                    //listView1.Items[itm.Index] = itm;
+                }
+            }
+        }*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                joinLobby(Convert.ToInt64(listView1.SelectedItems[0].SubItems[1].Text));
+            }
+            catch (Exception) { }
+        }
+
+        public static void joinLobby(long lobbyID)
+        {
+            System.Diagnostics.Process.Start("steam://joinlobby/730/" + Convert.ToString(lobbyID));
+            System.Console.WriteLine("Joined lobby " + Convert.ToString(lobbyID));
+
+        }
+
+
+        public static long getplobbyID(string player)
+        {
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create((player.Contains("/") ? "http://steamcommunity.com/" : "http://steamcommunity.com/id/") + player);
+                req.Method = "GET";
+                WebResponse res = req.GetResponse();
+                StreamReader read = new StreamReader(res.GetResponseStream(), System.Text.Encoding.UTF8);
+                string result = read.ReadToEnd();
+                read.Close();
+                res.Close();
+                total = total + 1;
+                if (!result.Contains("steam://joinlobby/730/"))
+                {
+                    System.Console.WriteLine("No Lobby Found for " + player);
+                    return 0;
+                }
+                string s1 = result.Split(new string[] { "steam://joinlobby/730/" }, StringSplitOptions.None)[1];
+                System.Console.WriteLine("Current lobbyID is " + s1.Split('/')[0]);
+                long lobID = Convert.ToInt64(s1.Split('/')[0]);
+                return lobID;
+            }
+            catch (Exception) { }
+            return 0;
+        }
+
+        private string appPath()
+        {
+            return Path.GetDirectoryName(Application.ExecutablePath);
+        }
+
+        private void loadList()
+        {
+            try
+            {
+                StreamReader read = new StreamReader(appPath() + "\\lobbylist.txt", Encoding.Default);
+                string line = "";
+                while (true)
+                {
+                    line = read.ReadLine();
+                    if (line == null)
+                    {
+                        read.Close();
+                        return;
+                    }
+                    addItem(line);
+                    System.Console.WriteLine(line);
+                }
+            }
+            catch (Exception) { }
+        }
+        private void saveList()
+        {
+            StreamWriter write = new StreamWriter(appPath() + "\\lobbylist.txt", true);
+            foreach (string[] s in players)
+            {
+                write.WriteLine(s[0]);
+            }
+            write.Close();
+            System.Console.WriteLine("Saved file to " + appPath());
+            sendmessage("Saved file to " + appPath()+ "\\lobbylist.txt");
+        }
+        bool key1down = false;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //Join Selected
+            if ((GetKeyState(0x0D) & 0x8000) > 0 && textBox1.Focused)
+            {
+                addItem(textBox1.Text);
+                sendmessage("Added " + textBox1.Text);
+                textBox1.Clear();
+            }
+            if (!checkBox1.Checked)
+                return;
+            if ((GetKeyState(0x61) & 0x8000) > 0)
+            {
+                if (!key1down)
+                {
+                    int ind = 0;
+                    try { ind = listView1.SelectedItems[0].Index; }
+                    catch (Exception) { }
+                    joinLobby(Convert.ToInt64(((String[])players[ind])[1]));
+                    key1down = true;
+                }
+            }
+            else key1down = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            busy = true;
+            while (working) { }
+            int ind = 0;
+            try { ind = listView1.SelectedItems[0].Index; }
+            catch (Exception) { busy = false; return; }
+            listView1.Items.RemoveAt(ind);
+            players.RemoveAt(ind);
+            update = true;
+            busy = false;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            saveList();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void labeltmr_Tick(object sender, EventArgs e)
+        {
+            
+            label1.Text = "";
+            labeltmr.Stop();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
